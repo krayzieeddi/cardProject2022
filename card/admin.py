8ba83141aa0faa5a2d5a105c00b8561dfc1af184
krayzie <edwinh300@gmail.com>
2022-03-21 14:53:18 -0700

    added in models

--- card/admin.py
+++ card/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CardType, Card
 
 # Register your models here.
+admin.site.register(CardType)
+admin.site.register(Card)
--- card/models.py
+++ card/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class CardType(models.Model):
+    cardTypeName=models.CharField(max_length=255)
+    cardTypeDescription=models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.cardTypeName
+    
+    class Meta:
+        db_table='cardType'
+        verbose_name_plural='cardTypes'
+
+class Card(models.Model):
+    cardName = models.CharField(max_length=255)
+    cardTypeId = models.ForeignKey(CardType, on_delete = models.DO_NOTHING)
+    cardDescription = models.TextField(max_length = 255, null = True, blank = True)
+
+    def __str__(self):
+        return self.cardName
+
+    class Meta:
+        db_table = 'card'
+        verbose_name_plural='cards'
