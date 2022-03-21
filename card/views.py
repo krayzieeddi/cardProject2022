2236fda227688e658053c89b892bf54829915683
krayzie <edwinh300@gmail.com>
2022-03-21 15:18:42 -0700

    setup view for card types

--- card/views.py
+++ card/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import CardType, Card
 
 # Create your views here.
 def index(request):
     return render(request, 'card/index.html')
+
+def getCardTypes(request):
+    cardType_list=CardType.objects.all()
+    return render(request, 'card/cardTypes.html' ,{'cardType_list' : cardType_list})
