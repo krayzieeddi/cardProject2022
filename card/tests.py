5a0948fd39d037af03e3acfb55d071054194726c
krayzie <edwinh300@gmail.com>
2022-03-23 13:25:27 -0700

    tests success

--- card/tests.py
+++ card/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import CardType, Card
+from django.urls import reverse_lazy, reverse
 
 # Create your tests here.
+class CardTypeTest(TestCase):
+    def setUp(self):
+        self.type = CardType(cardTypeName = 'cardType1')
+
+    def test_typestring(self):
+        self.assertEqual(str(self.type), 'cardType1')
+
+    def test_table(self):
+        self.assertEqual(str(CardType._meta.db_table), 'cardType')
+
+class CardTest(TestCase):
+    def setUp(self):
+        self.type = Card(cardName = 'card1')
+
+    def test_typestring(self):
+        self.assertEqual(str(self.type), 'card1')
+
+    def test_table(self):
+        self.assertEqual(str(Card._meta.db_table), 'card')
