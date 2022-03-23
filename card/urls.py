47c9f843a76bd0215bc774f682bf9f7b61d284a3
krayzie <edwinh300@gmail.com>
2022-03-23 13:09:54 -0700

    set up card details and cards

--- card/urls.py
+++ card/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('getCardTypes/', views.getCardTypes, name='cardTypes'),
+    path('getCard/', views.getCard, name='card'),
+    path('getCardDetail/<int:id>', views.getCardDetail, name='cardDetail'),
 ]
--- card/views.py
+++ card/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import CardType, Card
 
 # Create your views here.
@@ -8,3 +8,16 @@ def index(request):
 def getCardTypes(request):
     cardType_list=CardType.objects.all()
     return render(request, 'card/cardTypes.html' ,{'cardType_list' : cardType_list})
+
+def getCard(request):
+    card_list=Card.objects.all()
+    return render(request, 'card/card.html' ,{'card_list' : card_list})
+
+def getCardDetail(request, id):
+    card = get_object_or_404(Card, pk=id)
+    cardDetails = card.cardDescription
+    context = {
+        'card' : card,
+        'cardDetails' : cardDetails,
+    }
+    return render(request, 'card/cardDetail.html', context=context)
